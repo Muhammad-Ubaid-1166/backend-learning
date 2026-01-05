848cdf17afd251f0ce493bf3a25a0aab49d25c48
Muhammad Ubaid <ubaid123@gmail.com>
2026-01-05 10:54:22 +0500

    some code for Authentication

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,22 @@
+// const asynchandler = () => {
+
+
+
+
+
+
+// }
+
+export {asynchandler}
+
+
+const asynchandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
